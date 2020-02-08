ea487d70e3ed956508e630bfce925d7ff04477d9
Antonio Perez <anpesalado@gmail.com>
2020-02-08 01:29:49 +0100

    feat[modelos de bloque Expedientes]

---
+++ backend/persal/src/main/java/es/mcpg/persal/models/Factura.java
@@ -0,0 +1,44 @@
+package es.mcpg.persal.models;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name="facturas")
+public class Factura {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer id;
+	
+	@ManyToOne
+	@JoinColumn(name="id", nullable= false)
+	private Integer id_cuenta;
+	
+	@Column(name="fecha_emision")
+	private Date fecha_emision;
+	
+	@Column(name="descripcion")
+	private String descripcion;
+	
+	@Column(name="cantidad")
+	private Double cantidad;
+	
+	@Column(name="impuestos")
+	private Double impuestos;
+	
+	@Column(name="cantidad_total")
+	private Double cantidad_total;
+
+}
